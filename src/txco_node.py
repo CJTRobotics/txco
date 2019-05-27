0a569f006dfe8a6b03c33688dfb9627a9672779a
bionade24 <o.roesler@oscloud.info>
2019-05-27 16:32:55 +0200

    Moved Parameters into launchfile

--- src/txco_node.py
+++ src/txco_node.py
@@ -1,13 +1,22 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 import rospy
 from std_msgs.msg import Int16
 import time
 import serial
 
+
+def init():
+    rospy.init_node("txco_node", anonymous=False)
+
+def port():
+    return rospy.get_param('~port')
+
+def baudrate():
+    return rospy.get_param('~baudrate')
+
 def co2talker():
     pub = rospy.Publisher("txco_publisher", Int16, queue_size=10)
-    rospy.init_node("txco_node", anonymous=False)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
         co2_val = int(ser.readline())
@@ -16,10 +25,11 @@ def co2talker():
 
 if __name__ == "__main__":
     try:
-        ser = serial.Serial("/dev/ttyACM0", baudrate = 9600, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE, bytesize = serial.EIGHTBITS)
+        init()
+        ser = serial.Serial(port(), baudrate(), parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE, bytesize = serial.EIGHTBITS)
         time.sleep(1)
-        
         co2talker()
+        
     except rospy.ROSInterruptException:
         ser.close()
         pass
